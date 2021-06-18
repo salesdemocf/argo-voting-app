e6a2e3f24956b88de2f0b7a92fafa919e8842c89
Freshbot <salesdemocf@gmail.com>
2021-06-18 00:02:10 +0000

    update for SA-187-wolverine-or-toad

--- tests/selenium/test_app_vote.py
+++ tests/selenium/test_app_vote.py
@@ -32,7 +32,7 @@ def browser():
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
     option_a = "Jubilee"
-    option_a = "Beast"
+    option_a = "Wolverine"
     option_b = "Toad"
 
 def test_confirm_vote_choice_form(browser):
