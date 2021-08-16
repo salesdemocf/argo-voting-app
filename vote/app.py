d94a1aef2e0e4dc45639206f9a04f9f7a34d077a
Freshbot <salesdemocf@gmail.com>
2021-08-16 19:09:56 +0000

    update for SA-243-storm-or-mojo

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Gambit")
-option_b = os.getenv('OPTION_B', "Pyro")
+option_a = os.getenv('OPTION_A', "Storm")
+option_b = os.getenv('OPTION_B', "Mojo")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
