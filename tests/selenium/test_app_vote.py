e5d1a6e6a7a9ef75061595a2f78643484f99a058
Freshbot <salesdemocf@gmail.com>
2021-11-12 14:02:48 +0000

    update for SA-319-wolverine-or-sabretooth

--- tests/selenium/test_app_vote.py
+++ tests/selenium/test_app_vote.py
@@ -32,7 +32,7 @@ def browser():
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
     option_a = "Jubilee"
-    option_a = "Storm"
+    option_a = "Wolverine"
     option_b = "Sabretooth"
 
 def test_confirm_vote_choice_form(browser):
