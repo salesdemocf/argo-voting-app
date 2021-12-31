3839a16d18240df91803a8442d9ead2021a8d3b6
Freshbot <salesdemocf@gmail.com>
2021-12-31 14:03:27 +0000

    update for SA-353-storm-or-magneto

--- vote/app.py
+++ vote/app.py
@@ -5,7 +5,7 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Jubilee")
+option_a = os.getenv('OPTION_A', "Storm")
 option_b = os.getenv('OPTION_B', "Magneto")
 hostname = socket.gethostname()
 
