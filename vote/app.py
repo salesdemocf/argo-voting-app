da383cabcb9551ee249ed80de1cebb224cda3e02
Freshbot <salesdemocf@gmail.com>
2022-02-24 14:02:41 +0000

    update for SA-392-beast-or-magneto

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Beast")
-option_b = os.getenv('OPTION_B', "Toad")
+option_b = os.getenv('OPTION_B', "Magneto")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
