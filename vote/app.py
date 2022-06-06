43d04293168fe8c98a3a92f9cb310709d8688c68
Freshbot <salesdemocf@gmail.com>
2022-06-06 14:02:08 +0000

    update for SA-465-jubilee-or-mojo

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Jubilee")
-option_b = os.getenv('OPTION_B', "Juggernaut")
+option_b = os.getenv('OPTION_B', "Mojo")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
