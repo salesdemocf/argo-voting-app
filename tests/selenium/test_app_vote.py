6828101f98bf27631028de5cec0e03c1e4497166
Freshbot <salesdemocf@gmail.com>
2022-10-19 14:02:32 +0000

    update for SA-565-rogue-or-pyro

--- tests/selenium/test_app_vote.py
+++ tests/selenium/test_app_vote.py
@@ -32,7 +32,7 @@ def browser():
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
     option_a = "Jubilee"
-    option_a = "Beast"
+    option_a = "Rogue"
     option_b = "Pyro"
 
 def test_confirm_vote_choice_form(browser):
