bfeb0ab2b0dc6f909ee05a338d2afcfe08d19b0d
Freshbot <salesdemocf@gmail.com>
2023-06-02 14:03:26 +0000

    update for SA-728-rogue-or-toad

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Rogue")
-option_b = os.getenv('OPTION_B', "Juggernaut")
+option_b = os.getenv('OPTION_B', "Toad")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
