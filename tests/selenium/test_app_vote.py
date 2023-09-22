9d7dbcee32cd19385b0803f917087df03cb7624d
Freshbot <salesdemocf@gmail.com>
2023-09-22 14:03:26 +0000

    update for SA-807-storm-or-pyro

--- tests/selenium/test_app_vote.py
+++ tests/selenium/test_app_vote.py
@@ -32,8 +32,8 @@ def browser():
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
     option_a = "Jubilee"
-    option_a = "Cyclops"
-    option_b = "Mystique"
+    option_a = "Storm"
+    option_b = "Pyro"
 
 def test_confirm_vote_choice_form(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
