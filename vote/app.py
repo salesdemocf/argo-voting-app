03b9b25f6a4773fc28987c7ed377f80b48c2807b
Freshbot <salesdemocf@gmail.com>
2024-03-05 14:01:45 +0000

    update for SA-924-wolverine-or-sabretooth

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Jubilee")
-option_b = os.getenv('OPTION_B', "Mystique")
+option_a = os.getenv('OPTION_A', "Wolverine")
+option_b = os.getenv('OPTION_B', "Sabretooth")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
